e50f4d55f39f3b6ba9501233c548ac8eac7f49cf
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2026-06-26 20:59:15 +0300

    fix watching video logic in video seciton

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 function playVid() {
     let vid = document.getElementById("video");
+    let vidOverlay = document.getElementById("video-overlay");
+    let vidPlayer = document.getElementById("videoPlayer");
+    let vidIcon = document.getElementById("video-icon");
+    let vidText = document.getElementById("video-text");
 
     if (vid.paused) {
         vid.play();
+        vidOverlay.style.backgroundColor = "transparent";
+        vidPlayer.style.opacity = "0";
+        vidIcon.classList.replace("fa-video", "fa-pause");
+        vidText.innerHTML = "إيقاف الفيديو";
     } else {
         vid.pause();
+        vidOverlay.style.backgroundColor = "rgba(130, 77, 178, 0.9)";
+        vidPlayer.style.opacity = "100";
+        vidIcon.classList.replace("fa-pause", "fa-video");
+        vidText.innerHTML = "مشاهدة الفيديو";
     }
 }
 
@@ -13,24 +25,27 @@ player.addEventListener("click", playVid);
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
+    "use strict";
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+    var forms = document.querySelectorAll(".needs-validation");
 
     // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener(
+            "submit",
+            function (event) {
                 if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
+                    event.preventDefault();
+                    event.stopPropagation();
                 }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+                form.classList.add("was-validated");
+            },
+            false,
+        );
+    });
+})();
 
 function reserveRights(dateId) {
     document.getElementById(dateId).innerText = new Date().getFullYear();
